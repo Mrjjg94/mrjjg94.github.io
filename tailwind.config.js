c8ee6a7b9a838a5da4bc4bcdae87474f79513e6e
Josh Guerra <josh@Joshs-Mac-mini.lan>
2022-10-29 20:06:53 -0400

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: ["./pages/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        burtons: "burtons",
+      },
+    },
+  },
+  plugins: [],
+}
